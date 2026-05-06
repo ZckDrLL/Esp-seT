ecc08e6222ebbcc99528ac2378e295c06e9c459d
ZckDrLL <205637375+ZckDrLL@users.noreply.github.com>
2026-05-07 01:53:40 +0700

    смена внутреннего id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.esp32_nat_router_app"
+        applicationId = "com.zckdrll.esp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
